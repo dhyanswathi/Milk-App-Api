e82f2dfbce4a1700fa034bd38c026cda02264af1
swathi <swathysidharthan@gmail.com>
2023-01-24 11:31:53 +0100

    add db context

--- MilkApp.PopulateData/Program.cs
+++ MilkApp.PopulateData/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MilkApp.PopulateData.Models;
+
+IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureServices((context, services) =>
+{
+    var cns = "Data Source=LAPTOP-AC42D6SP\\MSSQLSERVER01;Initial Catalog=MilkDB;Integrated Security=True";
+    services.AddDbContext<MilkDBContext>(options => options.UseSqlServer(cns))
+    ;
+})
+.Build();
+
+host.Run();
