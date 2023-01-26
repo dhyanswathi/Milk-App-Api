f2e7ac160d0c193ef869210b75c950e23167baf7
swathi <swathysidharthan@gmail.com>
2023-01-26 18:32:48 +0100

    add httppost

--- Milk.Api/Models/IMilkRepository.cs
+++ Milk.Api/Models/IMilkRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Milk.Api.Data
+﻿
+namespace Milk.Api.Models
 {
     public interface IMilkRepository
     {
+        void Save();
+        Milk? GetOneById(string id);
+        IEnumerable<Milk> GetAll();
+        Milk Add(string name, string type, int? storage);
+        void Delete(string id);
+        Milk Update(string id, string name, string type, int? storage);
+
     }
 }
